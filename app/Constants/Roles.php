452dc654ac37be03e94e3023c4ca587301c23c2f
vahagnhov <vahagn505315v@gmail.com>
2023-09-17 19:49:11 +0400

    Install Laravel spatie permissions, create seeders for roles and admin

---
+++ app/Constants/Roles.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Constants;
+
+class Roles
+{
+    const ADMIN = 'admin';
+    const B2C_CUSTOMER = 'b2c_customer';
+    const B2B_CUSTOMER = 'b2b_customer';
+}
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Constants\Roles;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roles = [
+            Roles::ADMIN,
+            Roles::B2C_CUSTOMER,
+            Roles::B2B_CUSTOMER,
+        ];
+
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+    }
+}
